e1f6d3b68d40db0a4d75d0bff02cfbc2716dc1a9
Forthoney <castlehoneyjung@gmail.com>
2023-05-02 17:41:19 -0400

    update script

--- data/dataverse_download.py
+++ data/dataverse_download.py
@@ -1,7 +1,8 @@
+import re
 import sys
-import urllib.request
 
 import requests
+from tqdm import tqdm
 
 SERVER_URL = "https://researchdata.ntu.edu.sg"
 
@@ -9,14 +10,17 @@ if sys.argv[1] == "20":
     persistent_url = "doi:10.21979/N9/MA1AVG"
     version_no = "1.2"
     file_prefix = "20"
+    folder = "edf20"
 elif sys.argv[1] == "78":
     persistent_url = "doi:10.21979/N9/EUHGHS"
     version_no = "1.1"
     file_prefix = "78"
+    folder = "edf78"
 else:
     persistent_url = "doi:10.21979/N9/EAMYFO"
     version_no = "1.4"
     file_prefix = "shhs"
+    folder = "shhs"
 
 response = requests.get(
     f"{SERVER_URL}/api/datasets/:persistentId/?persistentId={persistent_url}"
@@ -31,7 +35,9 @@ datainfo = requests.get(
 datainfo = datainfo.json()
 
 data_ids = [datum["dataFile"]["id"] for datum in datainfo["data"]]
-for id in data_ids:
-    urllib.request.urlretrieve(
-        f"{SERVER_URL}/api/access/datafile/{id}", f"{file_prefix}_{id}"
-    )
+for id in tqdm(data_ids):
+    response = requests.get(f"{SERVER_URL}/api/access/datafile/{id}")
+    content_disposition = response.headers.get("Content-Disposition")
+    filename = re.search(r'filename="(.+)"', content_disposition).group(1)
+    with open(filename, "wb+") as f:
+        f.write(response.content)
