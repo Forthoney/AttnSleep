0bbdc03efe58d960d9b9325d42430e7e83aefff2
Forthoney <castlehoneyjung@gmail.com>
2023-05-01 21:05:13 -0400

    move base_trainer to tf

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -1,42 +1,43 @@
-import torch
 from abc import abstractmethod
-from numpy import inf
+
 import numpy as np
+import tensorflow as tf
+from numpy import inf
+
 
 class BaseTrainer:
     """
     Base class for all trainers
     """
+
     def __init__(self, model, criterion, metric_ftns, optimizer, config, fold_id):
         self.config = config
-        self.logger = config.get_logger('trainer', config['trainer']['verbosity'])
+        self.logger = config.get_logger("trainer", config["trainer"]["verbosity"])
 
-        # setup GPU device if available, move model into configured device
-        self.device, device_ids = self._prepare_device(config['n_gpu'])
-        self.model = model.to(self.device)
-        if len(device_ids) > 1:
-            self.model = torch.nn.DataParallel(model, device_ids=device_ids)
+        # TODO: ?setup GPU device if available, move model into configured device
+        self.device, device_ids = self._prepare_device(config["n_gpu"])
+        self.model = model
 
         self.criterion = criterion
         self.metric_ftns = metric_ftns
         self.optimizer = optimizer
 
-        cfg_trainer = config['trainer']
-        self.epochs = cfg_trainer['epochs']
-        self.save_period = cfg_trainer['save_period']
-        self.monitor = cfg_trainer.get('monitor', 'off')
+        cfg_trainer = config["trainer"]
+        self.epochs = cfg_trainer["epochs"]
+        self.save_period = cfg_trainer["save_period"]
+        self.monitor = cfg_trainer.get("monitor", "off")
         self.fold_id = fold_id
 
         # configuration to monitor model performance and save best
-        if self.monitor == 'off':
-            self.mnt_mode = 'off'
+        if self.monitor == "off":
+            self.mnt_mode = "off"
             self.mnt_best = 0
         else:
             self.mnt_mode, self.mnt_metric = self.monitor.split()
-            assert self.mnt_mode in ['min', 'max']
+            assert self.mnt_mode in ["min", "max"]
 
-            self.mnt_best = inf if self.mnt_mode == 'min' else -inf
-            self.early_stop = cfg_trainer.get('early_stop', inf)
+            self.mnt_best = inf if self.mnt_mode == "min" else -inf
+            self.early_stop = cfg_trainer.get("early_stop", inf)
 
         self.start_epoch = 1
 
@@ -62,29 +63,39 @@ class BaseTrainer:
         all_outs = []
         all_trgs = []
 
+        # Not very confident in checkpoint functionality ngl
+        checkpoint = tf.train.Checkpoint(model=self.model, optimizer=self.optimizer)
+
         for epoch in range(self.start_epoch, self.epochs + 1):
             result, epoch_outs, epoch_trgs = self._train_epoch(epoch, self.epochs)
 
             # save logged informations into log dict
-            log = {'epoch': epoch}
+            log = {"epoch": epoch}
             log.update(result)
             all_outs.extend(epoch_outs)
             all_trgs.extend(epoch_trgs)
             # print logged informations to the screen
             for key, value in log.items():
-                self.logger.info('    {:15s}: {}'.format(str(key), value))
+                self.logger.info("    {:15s}: {}".format(str(key), value))
 
             # evaluate model performance according to configured metric, save best checkpoint as model_best
-            best = False #True
-            if self.mnt_mode != 'off':
+            best = False  # True
+            if self.mnt_mode != "off":
                 try:
                     # check whether model performance improved or not, according to specified metric(mnt_metric)
-                    improved = (self.mnt_mode == 'min' and log[self.mnt_metric] <= self.mnt_best) or \
-                               (self.mnt_mode == 'max' and log[self.mnt_metric] >= self.mnt_best)
+                    improved = (
+                        self.mnt_mode == "min" and log[self.mnt_metric] <= self.mnt_best
+                    ) or (
+                        self.mnt_mode == "max" and log[self.mnt_metric] >= self.mnt_best
+                    )
                 except KeyError:
-                    self.logger.warning("Warning: Metric '{}' is not found. "
-                                        "Model performance monitoring is disabled.".format(self.mnt_metric))
-                    self.mnt_mode = 'off'
+                    self.logger.warning(
+                        "Warning: Metric '{}' is not found. "
+                        "Model performance monitoring is disabled.".format(
+                            self.mnt_metric
+                        )
+                    )
+                    self.mnt_mode = "off"
                     improved = False
 
                 if improved:
@@ -95,12 +106,15 @@ class BaseTrainer:
                     not_improved_count += 1
 
                 if not_improved_count > self.early_stop:
-                    self.logger.info("Validation performance didn\'t improve for {} epochs. "
-                                     "Training stops.".format(self.early_stop))
+                    self.logger.info(
+                        "Validation performance didn't improve for {} epochs. "
+                        "Training stops.".format(self.early_stop)
+                    )
                     break
 
             if epoch % self.save_period == 0:
-                self._save_checkpoint(epoch, save_best=best)
+
+                self._save_checkpoint(checkpoint, epoch, save_best=best)
 
         outs_name = "outs_" + str(self.fold_id)
         trgs_name = "trgs_" + str(self.fold_id)
@@ -114,20 +128,26 @@ class BaseTrainer:
         """
         setup GPU device if available, move model into configured device
         """
-        n_gpu = torch.cuda.device_count()
-        if n_gpu_use > 0 and n_gpu == 0:
-            self.logger.warning("Warning: There\'s no GPU available on this machine,"
-                                "training will be performed on CPU.")
+
+        n_gpus = len(tf.config.list_physical_devices('GPU'))
+        if n_gpu_use > 0 and n_gpus == 0:
+            self.logger.warning(
+                "Warning: There's no GPU available on this machine,"
+                "training will be performed on CPU."
+            )
             n_gpu_use = 0
-        if n_gpu_use > n_gpu:
-            self.logger.warning("Warning: The number of GPU\'s configured to use is {}, but only {} are available "
-                                "on this machine.".format(n_gpu_use, n_gpu))
-            n_gpu_use = n_gpu
-        device = torch.device('cuda:0' if n_gpu_use > 0 else 'cpu')
+        if n_gpu_use > n_gpus:
+            self.logger.warning(
+                "Warning: The number of GPU's configured to use is {}, but only {} are available "
+                "on this machine.".format(n_gpu_use, n_gpus)
+            )
+            n_gpu_use = n_gpus
+
+        device = tf.device("/gpu:0" if n_gpu_use > 0 else "/cpu:0")
         list_ids = list(range(n_gpu_use))
         return device, list_ids
 
-    def _save_checkpoint(self, epoch, save_best=True):
+    def _save_checkpoint(self, checkpoint: tf.train.Checkpoint, epoch, save_best=True):
         """
         Saving checkpoints
 
@@ -135,24 +155,16 @@ class BaseTrainer:
         :param log: logging information of the epoch
         :param save_best: if True, rename the saved checkpoint to 'model_best.pth'
         """
-        arch = type(self.model).__name__
-        state = {
-            'arch': arch,
-            'epoch': epoch,
-            'state_dict': self.model.state_dict(),
-            'optimizer': self.optimizer.state_dict(),
-            'monitor_best': self.mnt_best,
-            'config': self.config
-        }
-        filename = str(self.checkpoint_dir / 'checkpoint-epoch{}.pth'.format(epoch))
-        torch.save(state, filename)
+        filename = str(self.checkpoint_dir / "checkpoint-epoch{}.pth".format(epoch))
+
+        checkpoint.save(filename)
         self.logger.info("Saving checkpoint: {} ...".format(filename))
         if save_best:
-            best_path = str(self.checkpoint_dir / 'model_best.pth')
-            torch.save(state, best_path)
+            best_path = str(self.checkpoint_dir / "model_best.pth")
+            checkpoint.save(best_path)
             self.logger.info("Saving current best: model_best.pth ...")
 
-    def _resume_checkpoint(self, resume_path):
+    def _resume_checkpoint(self, checkpoint: tf.train.Checkpoint, resume_path):
         """
         Resume from saved checkpoints
 
@@ -160,33 +172,14 @@ class BaseTrainer:
         """
         resume_path = str(resume_path)
         self.logger.info("Loading checkpoint: {} ...".format(resume_path))
-        checkpoint = torch.load(resume_path)
-        self.start_epoch = checkpoint['epoch'] + 1
-        self.mnt_best = checkpoint['monitor_best']
-
-        # load architecture params from checkpoint.
-        if checkpoint['config']['arch'] != self.config['arch']:
-            self.logger.warning("Warning: Architecture configuration given in config file is different from that of "
-                                "checkpoint. This may yield an exception while state_dict is being loaded.")
-        self.model.load_state_dict(checkpoint['state_dict'])
-
-        # load optimizer state from checkpoint only when optimizer type is not changed.
-        if checkpoint['config']['optimizer']['type'] != self.config['optimizer']['type']:
-            self.logger.warning("Warning: Optimizer type given in config file is different from that of checkpoint. "
-                                "Optimizer parameters not being resumed.")
-        else:
-            self.optimizer.load_state_dict(checkpoint['optimizer'])
-
-        self.logger.info("Checkpoint loaded. Resume training from epoch {}".format(self.start_epoch))
+        checkpoint.restore(resume_path)
 
     def _calc_metrics(self):
-        from sklearn.metrics import classification_report
-        from sklearn.metrics import cohen_kappa_score
-        from sklearn.metrics import confusion_matrix
-        from sklearn.metrics import accuracy_score
-        import pandas as pd
         import os
-        from os import walk
+
+        import pandas as pd
+        from sklearn.metrics import (accuracy_score, classification_report,
+                                     cohen_kappa_score, confusion_matrix)
 
         n_folds = self.config["data_loader"]["args"]["num_folds"]
         all_outs = []
@@ -198,11 +191,11 @@ class BaseTrainer:
         for root, dirs, files in os.walk(save_dir):
             for file in files:
                 if "outs" in file:
-                     outs_list.append(os.path.join(root, file))
+                    outs_list.append(os.path.join(root, file))
                 if "trgs" in file:
-                     trgs_list.append(os.path.join(root, file))
+                    trgs_list.append(os.path.join(root, file))
 
-        if len(outs_list)==self.config["data_loader"]["args"]["num_folds"]:
+        if len(outs_list) == self.config["data_loader"]["args"]["num_folds"]:
             for i in range(len(outs_list)):
                 outs = np.load(outs_list[i])
                 trgs = np.load(trgs_list[i])
@@ -224,8 +217,7 @@ class BaseTrainer:
 
         cm_file_name = self.config["name"] + "_confusion_matrix.torch"
         cm_Save_path = os.path.join(save_dir, cm_file_name)
-        torch.save(cm, cm_Save_path)
-
+        np.save(cm_Save_path, cm)
 
         # Uncomment if you want to copy some of the important files into the experiement folder
         # from shutil import copyfile
@@ -235,6 +227,3 @@ class BaseTrainer:
         # copyfile("train_Kfold_CV.py", os.path.join(self.checkpoint_dir, "train_Kfold_CV.py"))
         # copyfile("config.json",  os.path.join(self.checkpoint_dir, "config.json"))
         # copyfile("data_loader/data_loaders.py",  os.path.join(self.checkpoint_dir, "data_loaders.py"))
-
-
-
